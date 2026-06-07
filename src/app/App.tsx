d0fe53376c17d98f2ebb0377c37ae4c70a29f6fd
patchwork-codebase <patchwork020@gmail.com>
2026-06-07 16:03:53 +0100

    feat: initialize App component with providers and integrate Cloudinary environment variables and Vercel analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "./components/auth/AuthContext";
 import { Toaster } from "sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./queryClient";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <AuthProvider>
         <RouterProvider router={router} />
         <Toaster position="bottom-right" richColors />
+        <Analytics />
       </AuthProvider>
     </QueryClientProvider>
   );
